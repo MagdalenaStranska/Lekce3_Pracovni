4470dd4729bd92bc7daedecd5f6440daab7e027f
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-08 00:54:11 +0200

    Pridavam soubory po vyuce

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ const pocetHodin = Number(prompt("Zadej pocet hodin zamestnance", "160"))
 const premie = Number(prompt("Zadej mimoradnou odmenu v Kc"))
 const plat = (hodinovka * pocetHodin) + premie
 
-document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"              
+document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"           
+
+const nazev = "Kniha 1"
+const autor = "Michal Kucera"
+const cena = 450
+const naSklade = true
+
+const kniha = {
+    nazev: "Kniha 1",
+    autor: "Michal Kucera",
+    cena: 450,
+    naSklade: true,
+}
+
+document.body.innerHTML += "<p>Kniha: " + nazev + ", autor: " + autor + "</p>"
+document.body.innerHTML += "<p>Kniha: " + kniha.nazev + ", autor: " + kniha.autor + "</p>"
+
